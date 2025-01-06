11affc21d953ff16669e42821d34719857f179e6
DLSiem <pdlsiem17@gmail.com>
2025-01-07 02:07:12 +0530

    Dutch national flag added

--- array_1.py
+++ array_1.py
@@ -15,15 +15,49 @@ def max_subarray_sum(arr):
     return max_sum
 
 # Test cases
+print("\n---Test cases for max_subarray_sum---")
 print(max_subarray_sum([1, 2, 3, 4, -1, -2, 5, 6]))  # 18
 print(max_subarray_sum([-2, -3, 4, -1, -2, 1, 5, -3]))  # 7
 print(max_subarray_sum([-2, -3, -4, -1, -2, -1, -5, -3]))  # -1
 print(max_subarray_sum([1, 2, 3, 4, -1, -2, 5, 6]))  # 18
 print(max_subarray_sum([-2, -3, 4, -1, -2, 1, 5, -3]))  # 7
 print(max_subarray_sum([-2, -3, -4, -1, -2, -1, -5, -3]))  # -1
+
 
 
 # Time complexity: O(n) 
 # Space complexity: O(1) 
 
- 
+
+"""2. Given an array consisting of three distinct values (e.g., 0, 1, 2), rearrange the array such that all instances of:
+
+    The first value (e.g., 0) appear at the beginning.
+    The second value (e.g., 1) appear in the middle.
+    The third value (e.g., 2) appear at the end.
+ """
+
+def sort_array(arr):
+    low = 0
+    mid = 0
+    high = len(arr) - 1
+
+    while mid <= high:
+        if mid == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif mid == 1:
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+
+    return arr
+
+# Test cases
+print("\n\n---Test cases for sort_array---")
+print(sort_array([0, 1, 2, 0, 1, 2]))  # [0, 0, 1, 1, 2, 2]
+print(sort_array([2, 1, 0, 2, 1, 0]))  # [0, 0, 1, 1, 2, 2]
+print(sort_array([2, 1, 0, 0, 1, 2]))  # [0, 0, 1, 1, 2, 2]
+print(sort_array([2, 1, 0, 0, 1, 2]))  # [0, 0, 1, 1, 2, 2]
+print(sort_array([0, 1, 2, 0, 1, 2]))  # [0, 0, 1, 1, 2, 2]
