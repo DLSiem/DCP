f7b17873cad92178c7f68db4cba96c4938fe4202
DLSiem <pdlsiem17@gmail.com>
2025-01-09 03:09:47 +0530

    insertion sort inseted

--- sorting.py
+++ sorting.py
@@ -1,7 +1,36 @@
+# insertion sort
+
+def insertion_sort(arr):
+    n = len(arr)
+
+    for i in range(1, n):
+        key = arr[i]
+        j = i-1
+
+        while j>=0 and key<arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+# test cases
+# test case 1
+print("\n------Insertion sort--------")
+arr = [64, 25, 12, 22, 11]
+insertion_sort(arr)
+# Expected output: [11, 12, 22, 25, 64]
+print(arr)
+
+# test case 2
+arr = [5, 2, 1, 3, 4]
+insertion_sort(arr)
+# Expected output: [1, 2, 3, 4, 5]
+print(arr)
+
+
 
 
 # selection sort
-
 def selection_sort(arr):
     n = len(arr)
 
@@ -14,6 +43,7 @@ def selection_sort(arr):
 
 # test cases 
 # test case 1
+print("\n------Selection sort--------")
 arr = [64, 25, 12, 22, 11]
 selection_sort(arr)
 # Expected output: [11, 12, 22, 25, 64]
@@ -43,6 +73,7 @@ def bubble_sort(arr):
 
 # test cases
 # test case 1
+print("\n------Bubble sort--------")
 arr = [64, 25, 12, 22, 11]
 bubble_sort(arr)
 # Expected output: [11, 12, 22, 25, 64]
