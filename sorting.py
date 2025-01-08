67b2b51a80d0d1d99df9f0c14affcebc1b7d7f9d
DLSiem <pdlsiem17@gmail.com>
2025-01-09 02:35:36 +0530

    bubble sort added

--- sorting.py
+++ sorting.py
@@ -24,3 +24,32 @@ arr = [5, 2, 1, 3, 4]
 selection_sort(arr)
 # Expected output: [1, 2, 3, 4, 5]
 print(arr)
+
+# bubble sort
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n-1):
+        swapped = False
+
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        
+        if swapped == False:
+            break
+    return arr
+
+# test cases
+# test case 1
+arr = [64, 25, 12, 22, 11]
+bubble_sort(arr)
+# Expected output: [11, 12, 22, 25, 64]
+print(arr)
+
+# test case 2
+arr = [5, 2, 1, 3, 4]
+bubble_sort(arr)
+# Expected output: [1, 2, 3, 4, 5]
+print(arr)
